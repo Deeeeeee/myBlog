f12bd9391b34288e88f45c4c45d882b2ce287b79
zhangdi <1325506619@qq.com>
2016-11-03 14:08:52 +0800

    gulp sass

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+"use strict";
+var gulp = require('gulp');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+
+var path = {
+	sass:["static/sass/**/*.scss"],
+	css:['static/css/**/*']
+};
+
+
+// gulp.task('js',function  () {
+// 	return	gulp.src(path.js)
+// 		.pipe(uglify())
+// 		.pipe(gulp.dest('static/js'))
+// });
+
+
+gulp.task('css',function  () {
+	return	gulp.src(path.css)
+		.pipe(gulp.dest('static/css'))
+});
+
+gulp.task('sass',function () {
+	return 	gulp.src(path.sass)
+		.pipe(sass())
+		.pipe(minifyCss({
+			keepSpecialComments: 0
+		}))
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(gulp.dest('public' + '/css'));
+});
+
+gulp.task('watch',["sass"],function () {
+	gulp.watch(path.sass,['sass']);
+});
+
+gulp.task('default', ['watch']);
