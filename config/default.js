82e2eee91b2bbed21d3c7506d87720d1c14c41b1
blacker <ashitudou@126.com>
2016-12-27 14:10:27 +0800

    storge

--- config/default.js
+++ config/default.js
@@ -7,6 +7,12 @@ module.exports = {
     },
 
     //mongodb: 'mongodb://192.168.0.185:27017/blog'
-    mongodb: 'mongodb://127.0.0.1:27017/blog'
+    mongodb: 'mongodb://127.0.0.1:27017/blog',
     // mongodb: 'mongodb://dee:admin000@ds133358.mlab.com:33358/deeblog'
+    storge:{
+        ACCESS_KEY:'TpziYLoGsAsk4krKqqpztKvfnPJGrlrLHMbUicOa',
+        SECRET_KEY:'O26kXctAMZCvzkxjjihgE21vJZcz_WEuAthDWC8_',
+        bucket :'demyblog',
+        domian : 'http://oisbtzf5h.bkt.clouddn.com'
+    }
 };
