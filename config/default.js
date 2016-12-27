592ae67925e2747ddaa3767092d6e10b162734f8
Deeeeeee <445214770@qq.com>
2016-12-27 11:47:46 +0800

    css

--- config/default.js
+++ config/default.js
@@ -5,7 +5,6 @@ module.exports = {
         key: 'blog',
         maxAge: 2592000000
     },
-
-    mongodb: 'mongodb://192.168.0.101:27017/blog'
+    mongodb: 'mongodb://192.168.0.46:27017/blog'
     // mongodb: 'mongodb://dee:admin000@ds133358.mlab.com:33358/deeblog'
 };
