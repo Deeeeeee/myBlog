3b17411db42f32955c3d3b294a524cc7179a2f5c
Deeeeeee <445214770@qq.com>
2016-12-29 11:28:48 +0800

    评论标记

--- models/comment.js
+++ models/comment.js
@@ -16,14 +16,20 @@ module.exports = {
         return Comment.update({ _id: commentId },{$set: data}).exec();
     },
 
+    // 通过评论 id 屏蔽一个评论
+    readCommentById: function (commentId, data) {
+        return Comment.update({ _id: commentId },{$set: data}).exec();
+    },
+
     // 通过文章 id 删除该文章下所有评论
     delCommentsByArticleId: function (articleId) {
         return Comment.remove({ articleId: articleId }).exec();
     },
 
-    getComments: function () {
+    // 通过 阅读标记 获取评论   （0 未读  1 已读）
+    getCommentsByReadStatus: function (readStatus) {
         return Comment
-            .find()
+            .find({readStatus: readStatus})
             .populate({ path: 'author', model: 'User' })
             .sort({ _id: -1 })
             .addCreatedAt()
---
+++ public/js/page/userCenter.js
@@ -0,0 +1,46 @@
+define(["jquery", "notie"], function ($, notie) {
+    var page = {
+        init: function () {
+            this.render();
+            this.bindEvents();
+        },
+        render: function () {
+            this.initUserInfo()
+        },
+        bindEvents: function () {
+            this.onReadComment();
+        },
+
+        initUserInfo: function () {
+
+        },
+        onReadComment: function () {
+            $(".J_finishRead").on("click", function () {
+                var _this = $(this);
+                var commentId = _this.attr("data-id");
+                var data = {
+                    commentId: commentId,
+                    // authorId: authorId
+                };
+                $.ajax({
+                    type: 'post',
+                    data: data,
+                    url: '/readComment',
+                    success: function (data) {
+                        if (data.code === 0) {
+                            _this.parent().hide();
+                        } else {
+                            alert(data.message);
+                        }
+                    },
+                    error: function (err) {
+                        console.log(err);
+                    }
+                })
+            });
+        }
+    };
+    page.init();
+
+
+});
